a4d335dd05ccb2be3cdbbad48216b0d948e21a59
Ben Smith <benjamin.smith@sky.uk>
2017-08-01 23:47:07 +0100

    Fixed the static files in the root for production.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,32 +1,38 @@
 import * as express from 'express';
-import {Router} from 'express';
+import {Request, Response, Router} from 'express';
 import {json} from './models/PropertiesModel';
 import * as cors from 'cors';
+import * as path from 'path';
 
 const app = express();
+app.set('port', process.env.PORT || 4001);
+
 app.use(cors());
 
-app.set("port", process.env.PORT || 4001);
-
-if (process.env.NODE_ENV === "production") {
-    // server the react app in production mode
-    app.use('/static', express.static(__dirname + '/static'));
-    app.get('/', function(req, res){
-        res.sendFile(__dirname + '/index.html');
-    });
-}
 
 let router: Router;
 router = Router();
-router.route('/status').get((req, res) => {
+router.route('/status').get((req: Request, res: Response) => {
     res.send('We are running 100%');
 });
-router.route('/properties').get((req, res) => {
+router.route('/properties').get((req: Request, res: Response) => {
     res.json(json);
 });
-
 app.use('/api', router);
 
-app.listen(app.get("port"), function () {
-    console.log(`react-host-maker app running in ${process.env.NODE_ENV} env. Listening on port ${app.get("port")}`)
+
+if (process.env.NODE_ENV === 'production') {
+    // server the react app in production mode
+    app.use('/static', express.static(path.resolve(__dirname + '/../static')));
+    app.get('/', (req: Request, res: Response) => {
+        res.sendFile(path.resolve(__dirname + '/../index.html'));
+    });
+    app.get(/^(.+)$/, (req: Request, res: Response) => {
+        let resolved = path.resolve(__dirname + '/..' + req.originalUrl);
+        res.sendFile(resolved);
+    });
+}
+
+app.listen(app.get('port'), () => {
+    console.log(`react-host-maker app running in ${process.env.NODE_ENV} env. Listening on port ${app.get('port')}`)
 });
