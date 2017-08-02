3263dc9851ebd046220f76113828ecc51b58202a
Ben Smith <benjamin.smith@sky.uk>
2017-08-02 23:18:33 +0100

    Step2 really finish now, needed to be able to update on failed address and correctly find address in serviceable area. Some more style for input. DONE

--- src/client/component/search/Search.js
+++ src/client/component/search/Search.js
@@ -10,34 +10,46 @@ class Search extends Component {
         window.alert('You\'ll have to hire me before you can add it to your list');
     }
 
-    render() {
-        const {searchGoogle, search} = this.props;
-
-        let resultPanel = '';
-
+    notificationPanel(search) {
         if (search && search.formattedAddress) {
-            resultPanel = <div>
-                <div className="searchedAddress found">
-                    <p>{search.formattedAddress}</p>
-                    <p>Yes we can service your property</p>
+            if (search.formattedAddress && search.serviceable) {
+                return <div>
+                    <div className="searchedAddress found">
+                        <p>{search.formattedAddress}</p>
+                        <p>Yes we can service your property</p>
+                    </div>
+                    <form>
+                        <input type="button" value="Add Now" onClick={this.addProperty.bind(this)}/>
+                    </form>
                 </div>
-                <form>
-                    <input type="button" value="Add Now" onClick={this.addProperty.bind(this)}/>
-                </form>
+            } else {
+                return <div className="searchedAddress notFound">
+                    <p>{search.formattedAddress}</p>
+                    <p>Unfortunately, we can not service that property at the moment</p>
+                </div>
+            }
+        } else if (search && search.error) {
+            return <div className="searchedAddress notFound">
+                <p>We could not find that address</p>
             </div>
         } else {
-            resultPanel = <div className="searchedAddress notFound">
-                <p>{search.formattedAddress}</p>
-                <p>Unfortunately, we can not service that property at the moment</p>
+            return <div className="searchedAddress start">
+                <p>Try an address or postcode</p>
             </div>
         }
+    }
+
+    render() {
+        const {searchGoogle, search} = this.props;
 
         return (
             <div className="row search">
                 <div className="col-lg-offset-1 col-lg-5 search-panel">
                 <h3>Search Address</h3>
                 <SearchInput handelSubmit={searchGoogle}/>
-                {resultPanel}
+                {
+                    this.notificationPanel(search)
+                }
                 </div>
             </div>
         )
--- src/client/store/search/searchReducer.js
+++ src/client/store/search/searchReducer.js
@@ -11,6 +11,8 @@ const searchReducer = (state = initialState, action = {}) => {
     switch (action.type) {
         case types.SEARCH_GOOGLE_SUCCESS:
             return {...action.search};
+        case types.SEARCH_GOOGLE_FAILED:
+            return {...{error: action.error}};
         default:
             return state;
     }
